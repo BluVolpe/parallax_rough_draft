a9c995b844ff8ded4e4e899142ad92524654bda1
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-07-18 23:22:27 -0400

    finished parallax // footer missing // content  images bugged

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
-timeline.to('.text', 5, {x: 500});
+timeline
+    .to(".rock", 5, {y: -400})
+    .to(".girl", 5, {y: -250}, "-=5")
+    .to(".bg1", 5, {y:100}, "-=5")
+    .to(".content", 5, {top:"3%"}, "-=5")
+    .fromTo(".content-images", {opacity:0}, {opacity:1, duration:1})
+
+
+    let scene = new ScrollMagic.Scene({
+        triggerElement: "section",
+        duration: "100%",
+        triggerHook: 0,
+    })
+        .setTween(timeline)
+        .setPin("section")
+        .addTo(controller);
