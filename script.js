566ab8c835184af8e035c2884e347fcb18af00e7
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-07-21 00:51:54 -0400

    fixed mobile parallax

--- script.js
+++ script.js
@@ -2,20 +2,19 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-    .to(".bluebox0", 5, {y: -1050})
-    .to(".bluebox1", 5, {y: -700}, "-=5")
-    .to(".bluebox2", 5, {y: -600}, "-=5")
-    .to(".bluebox3", 5, {y: -250}, "-=5")
-    .to(".blueboxB", 5, {y:00}, "-=5")
-    .to(".content", 5, {top:"3%"}, "-=5")
-    .fromTo(".content-images", {opacity:0}, {opacity:1, duration:1})
+  .to(".bluebox0", 5, { y: -800 })
+  .to(".bluebox1", 5, { y: -600 }, "-=5")
+  .to(".bluebox2", 5, { y: -500 }, "-=5")
+  .to(".bluebox3", 5, { y: -250 }, "-=5")
+  .to(".blueboxB", 5, { y: 00 }, "-=5")
+  .to(".content", 5, { top: "3%" }, "-=5")
+  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 1 });
 
-
-    let scene = new ScrollMagic.Scene({
-        triggerElement: "section",
-        duration: "100%",
-        triggerHook: 0,
-    })
-        .setTween(timeline)
-        .setPin("section")
-        .addTo(controller);
+let scene = new ScrollMagic.Scene({
+  triggerElement: "section",
+  duration: "100%",
+  triggerHook: 0,
+})
+  .setTween(timeline)
+  .setPin("section")
+  .addTo(controller);
